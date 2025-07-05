2149038fbfa90fc47d41a17b787f8554902e0901
ceirancat <meowmeowfeline202@gmail.com>
2025-07-05 16:46:23 +0800

    Add files via upload

---
+++ pokemon legends.py
@@ -0,0 +1,83 @@
+from random import *
+print("Ceiran:")
+print("Welcome to the final battle of the Legend league.")
+print("I am Ceiran, the founder of the league")
+print("You are going to use a Legendary Pokemon to beat Ayatsugi, the champion of the league.")
+print("Ayatsugi:")
+print("Please, what is your name?")
+name=input()
+print("Ayatsugi:")
+print("Wow, that's a great name,",name,"!")
+print("Let us start the battle now.")
+print("Please, choose a Pokemon. Type in lowercase only.")
+user=input()
+if user=="mewtwo":
+    print("Looks like you have chosen Mewtwo, the Genetic Pokemon.")
+elif user=="mew":
+    print("Looks like you have chosen Mew, the New Species Pokemon.")
+elif user=="lugia":
+    print("Looks like you have chosen Lugia, the Diving Pokemon.")
+elif user=="hooh" or "ho-oh":
+    print("Looks like you have chosen Ho-oh, the Rainbow Pokemon.")
+elif user=="entei":
+    print("Looks like you have chosen Entei, the Volcano Pokemon.")
+elif user=="raikou":
+    print("Looks like you have chosen Raikou, the Thunder Pokemon.")
+elif user=="suicune":
+    print("Looks like you have chosen Suicune, the Aurora Pokemon.")
+elif user=="celebi":
+    print("Looks like you have chosen Celebi, the Time Travel Pokemon.")
+elif user=="groudon":
+    print("Looks like you have chosen Groudon, the Continental Pokemon.")
+elif user=="kyogre":
+    print("Looks like you have chosen Kyogre, the Sea Basin Pokemon.")
+elif user=="rayquaza":
+    print("Looks like you have chosen Rayquaza, the Sky High Pokemon.")
+elif user=="latias" or "latios":
+    print("Looks like you have chosen The Eon Duo! Congrajulations for unlocking HELL MODE.")
+elif user=="deoxys":
+    print("Looks like you have chosen Deoxys, the DNA Pokemon.")
+elif user=="jirachi":
+    print("Looks like you have chosen Ji... Oh dear! She is still asleep, and too unconscious to battle! Congrajulations on unlocking DEPRIVATION MODE.")
+elif user=="palkia":
+    print("Looks like you have chosen Palkia, the Space Pokemon.")
+elif user=="dialga":
+    print("Looks like you have chosen Dialga, the Time Pokemon.")
+elif user=="giratina":
+    print("Looks like you have chosen Giratina, the Antimatter Pokemon.")
+elif user=="arceus":
+    print("The God does not want to fight. Congrajulations on unlocking MYTH MODE.")
+elif user=="darkrai":
+    print("Looks like you have chosen Darkrai, the Nightmare Pokemon.")
+elif user=="cresselia":
+    print("Looks like you have chosen Cresselia, the Lunar Pokemon.")
+elif user=="zekrom":
+    print("Looks like you have chosen Zekrom, the Deep Black Pokemon.")
+elif user=="reshiram":
+    print("Looks like you have chosen Reshiram, the Vast White Pokemon.")
+elif user=="kyurem":
+    print("Looks like you have chosen Kyurem, the Boundary Pokemon.")
+elif user=="diancie":
+    print("Looks like you have chosen Diancie, the Jewel Pokemon.")
+elif user=="xerneas":
+    print("Looks like you have chosen Xerneas, the Life Pokemon.")
+elif user=="yveltal":
+    print("Looks like you have chosen Yveltal, the Destruction Pokemon. You are completely destroyed and warped to another world. Congrajulations on unlocking REBIRTH MODE.")
+elif user=="":
+    print("Looks like you have chosen Mew, the New Species Pokemon.")
+elif user=="mew":
+    print("Looks like you have chosen Mew, the New Species Pokemon.")
+elif user=="mew":
+    print("Looks like you have chosen Mew, the New Species Pokemon.")
+elif user=="mew":
+    print("Looks like you have chosen Mew, the New Species Pokemon.")
+elif user=="mew":
+    print("Looks like you have chosen Mew, the New Species Pokemon.")
+elif user=="mew":
+    print("Looks like you have chosen Mew, the New Species Pokemon.")
+elif user=="mew":
+    print("Looks like you have chosen Mew, the New Species Pokemon.")
+elif user=="mew":
+    print("Looks like you have chosen Mew, the New Species Pokemon.")
+
+
